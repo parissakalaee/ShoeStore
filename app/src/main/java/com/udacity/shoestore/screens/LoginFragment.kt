b4df44254708308a218f59256347ac446bfdf80d
parissa <par4301@yaboo.com>
2020-12-30 17:33:31 +0100

    Step 10-1
    I tried to delete actionbar in LoginFragment.kt

--- app/src/main/java/com/udacity/shoestore/screens/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/LoginFragment.kt
@@ -1,20 +1,23 @@
 package com.udacity.shoestore.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
+
 class LoginFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.actionBar?.hide()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,9 +30,6 @@ class LoginFragment : Fragment() {
                 container,
                 false
             )
-
-//        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
-//        binding.loginViewModel = viewModel
 
         binding.signInButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment)
